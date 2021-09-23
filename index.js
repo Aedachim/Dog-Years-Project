aae0f0ef965e3518cda1eacd2711b3a40a4fb797
Aedachim <77279860+Aedachim@users.noreply.github.com>
2021-09-23 18:35:32 +0100

    Add files via upload
    Created with JavaScript

---
+++ index.js
@@ -0,0 +1,16 @@
+// My currently age 
+const myAge = 29;
+/* The first 2 years of a human is in dog life 10.5 x 2 */
+let earlyYears = 2;
+earlyYears *= 10.5;
+// Currently human age subtract - 2
+let laterYears = myAge - 2;
+// 4 years for a human is  1 dog year
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+// My currently age 29 in dog years result
+let myAgeInDogYears = earlyYears + laterYears
+const myName = "Tom".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
